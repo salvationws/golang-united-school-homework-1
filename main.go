11f1d95e7590531d8864dc079b1e8f854a982481
George Magradze <gsmagradze@protonmail.com>
2022-02-16 01:43:15 +0400

    reworked

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/kyokomi/emoji/v2"
 )
 
 func main() {
-	emoji.Println("Hello :world_map: !")
+	worldEmoji := emoji.Sprint("Hello :world_map:!")
+	fmt.Println(worldEmoji)
 }
